f1ab9818cfe7d7605683d0cc1e3166479d87c99f
v-ruvick <v-ruvick@MININT-RITN9RO>
2017-05-22 11:00:31 -0700

    Add project files.

---
+++ SlideShow/MainPage.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using System.Linq;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SlideShow
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+    public sealed partial class MainPage : Page
+    {
+        private ConcurrentBag<string> blobUris = new ConcurrentBag<string>();
+        private ConcurrentBag<string> collection = new ConcurrentBag<string>();
+        private ConcurrentBag<string> slideShowBlogUris = new ConcurrentBag<string>();
+
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=iotslideshowvisuals;AccountKey=yY8q5u5aCbD4RTDS5D4JAihP+0K2c3PLcQIxDwXMlK1SFrqW2tgOcxQk4TCFArKsVsi5Tp6AGH961Nyiu88zWQ==;EndpointSuffix=core.windows.net");
+        BlobContinuationToken continuationToken = null;
+        CloudBlobClient blobClient;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int change = 1;
+
+        string[] fileExtensions = { ".jpg", ".png", ".jpeg", ".gif", ".mp4", ".bmp", ".tiff" };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        async private void Page_Load(object sender, RoutedEventArgs e)
+        {
+            hideCollectionShowSlideShow();
+
+            await initCollection();
+        }
+
+        public void scrollThroughSlideShow()
+        {
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += (o, a) =>
+            {
+                // If we'd go out of bounds then start from the beginning
+                int newIndex = flipView.SelectedIndex + change;
+
+                if (newIndex >= flipView.Items.Count || newIndex < 0)
+                {
+                    change *= -1;
+                }
+
+                flipView.SelectedIndex += change;
+            };
+            timer.Start();
+        }
+
+        private void hideCollectionShowSlideShow()
+        {
+            listView.Visibility = Visibility.Collapsed;
+            textBlock.Visibility = Visibility.Collapsed;
+
+            flipView.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        ///Initializes a list of containers within the a blob. 
+        /// </summary>
+        /// <returns></returns>
+        async private Task initBlogClient()
+        {
+            blobClient = storageAccount.CreateCloudBlobClient();
+
+            ContainerResultSegment resultSegment = null;
+
+            resultSegment = await blobClient.ListContainersSegmentedAsync(
+            "", ContainerListingDetails.Metadata, 5, continuationToken, null, null);
+            foreach (CloudBlobContainer container in resultSegment.Results)
+            {
+                collection.Add(container.Name);
+            }
+            listView.ItemsSource = collection;
+        }
+
+        /// <summary>
+        /// Initializes a specific collection in the blob storage and will automatically start slide the slide show
+        /// for all images in that blob container.
+        /// </summary>
+        /// <returns></returns>
+        async private Task initCollection()
+        {
+            blobClient = storageAccount.CreateCloudBlobClient();
+            BlobResultSegment resultSegment = null;
+            CloudBlobContainer container = blobClient.GetContainerReference("space");
+
+            do
+            {
+                resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+                foreach (var blobItem in resultSegment.Results)
+                {
+                    if (fileExtensions.Any(blobItem.StorageUri.PrimaryUri.ToString().Contains))
+                    {
+                        blobUris.Add(blobItem.StorageUri.PrimaryUri.ToString());
+                    }
+                }
+                continuationToken = resultSegment.ContinuationToken;
+            }
+            while (continuationToken != null);
+            startSlideShow();
+        }
+
+        private void showCollectionHideSlideshow()
+        {
+            listView.Visibility = Visibility.Visible;
+            textBlock.Visibility = Visibility.Visible;
+
+            flipView.Visibility = Visibility.Collapsed;
+        }
+
+        async private Task loadImages(string collectionClicked)
+        {
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(collectionClicked);
+            BlobResultSegment resultSegment = null;
+            //refresh bloburis
+            blobUris = new ConcurrentBag<string>();
+            do
+            {
+                resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+                foreach (var blobItem in resultSegment.Results)
+                {
+                    if (fileExtensions.Any(blobItem.StorageUri.PrimaryUri.ToString().Contains))
+                    {
+                        blobUris.Add(blobItem.StorageUri.PrimaryUri.ToString());
+                    }
+                }
+                continuationToken = resultSegment.ContinuationToken;
+            }
+            while (continuationToken != null);
+            popupGridView.ItemsSource = blobUris;
+        }
+
+        private void startSlideShow()
+        {
+            if(slideShowBlogUris.IsEmpty)
+            {
+                flipView.ItemsSource = blobUris;
+            }
+            else
+            {
+                flipView.ItemsSource = slideShowBlogUris;
+            }
+
+            scrollThroughSlideShow();
+        }
+
+        async private void listView_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            listView.Visibility = Visibility.Collapsed;
+            textBlock.Visibility = Visibility.Collapsed;
+
+            ImagePopup.IsOpen = true;
+
+            if (e.OriginalSource.GetType() == typeof(TextBlock))
+            {
+                TextBlock item = (TextBlock)e.OriginalSource;
+                await loadImages(item.DataContext.ToString());
+            }
+            else
+            {
+                ListViewItemPresenter item = (ListViewItemPresenter)e.OriginalSource;
+                await loadImages(item.Content.ToString());
+            }
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void pause_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void StartShowClicked(object sender, RoutedEventArgs e)
+        {
+            ImagePopup.IsOpen = false;
+            hideCollectionShowSlideShow();
+            startSlideShow();
+        }
+
+        private void ImagePopup_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //showCollectionHideSlideshow();
+
+            //When popup has gone out of focus then clear image source
+            //blobUris = new ConcurrentBag<string>();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if(e.OriginalSource.GetType() == typeof(CheckBox))
+            {
+                CheckBox cb = sender as CheckBox;
+                string uri = cb.DataContext.ToString();
+                slideShowBlogUris.Add(uri);
+            }
+        }
+
+        async private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Go back to main menu
+            showCollectionHideSlideshow();
+            slideShowBlogUris = new ConcurrentBag<string>();
+            await initBlogClient();
+        }
+
+        private void Grid_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            button.Visibility = Visibility.Visible;
+        }
+
+        private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            button.Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
